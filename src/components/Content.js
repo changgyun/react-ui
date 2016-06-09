576592e8c072b9e370ef4c4d9f524a524c907e47
changgyun.woo <cg25.woo@samsung.com>
2016-06-09 18:52:26 +0900

    re

--- src/components/Content.js
+++ src/components/Content.js
@@ -16,7 +16,7 @@ class Contacts extends React.Component {
             contactData: [
                 {name: "Abet", phone: "010-0000-0001"},
                 {name: "Betty", phone: "010-0000-0002"},
-                {name: "Charlie", phone: "010-0000-0003"},
+                {name: "Chaascascarlie", phone: "010-0000-0003"},
                 {name: "David", phone: "010-0000-0004"}
             ]
         };
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,71 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    devtool: 'source-map',
+    entry: ['./src/index.js'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+            },
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        inline: true,
+        port: 3000,
+        contentBase: __dirname + '/dist/',
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: 'css-loader?sourceMap!sass-loader?sourceMap=true&sourceMapContents=true'
+            }
+        ]
+    }
+}
+
+/*
+var path = require('path')
+var webpack = require('webpack')
+
 module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: __dirname + '/public/',
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+            },
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ],
 
     devServer: {
         inline: true,
@@ -23,7 +84,7 @@ module.exports = {
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 }
-            }
+            },
         ]
     }
-};
+};*/
