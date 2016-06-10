f10e08949365532b59c585b25910b5e25e6193ed
changgyun.woo <cg25.woo@samsung.com>
2016-06-10 19:37:15 +0900

    ff

--- src/components/Button.js
+++ src/components/Button.js
@@ -16,3 +16,39 @@ const MyAwesomeReactComponent = () => (
 );
 
 export default MyAwesomeReactComponent;
+/*
+
+import React from 'react';
+import { AppBar } from 'material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
+*/
+/*
+ import getMuiTheme from 'material-ui/styles/getMuiTheme';
+ import { CustomTheme } from '../layout/themeCustom';*//*
+
+
+
+const style = {
+    margin: 12,
+};
+
+class Button extends React.Component {
+    */
+/* getChildContext() {
+     return {muiTheme: getMuiTheme(CustomTheme)};
+     }*//*
+
+
+    render () {
+        return
+        <div>
+            <h2>Button</h2>
+            <RaisedButton label="Default" style={style} className="muidocs-checkbox-example" />
+            <RaisedButton label="Primary" primary={true} style={style} />
+            <RaisedButton label="Secondary" secondary={true} style={style} />
+            <RaisedButton label="Disabled" disabled={true} style={style} />
+        </div>
+    }
+}
+
+export default Button;*/
