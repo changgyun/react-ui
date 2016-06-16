3088821f297d180bcb150f5444711c0b469eea18
changgyun.woo <cg25.woo@samsung.com>
2016-06-16 18:33:32 +0900

    set

---
+++ webpack.config.prod.js
@@ -0,0 +1,57 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: ['./src/index'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new ExtractTextPlugin("style.css", {
+            allChunks: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+            },
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+    devServer: {
+        inline: true,
+        port: 3000,
+        contentBase: __dirname + '/dist/',
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            },
+        ]
+    }
+}
