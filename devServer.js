ea7d117ddca643a75c0b36f563cbeaf0e399c442
changgyun.woo <cg25.woo@samsung.com>
2016-06-17 13:24:19 +0900

    depl

---
+++ devServer.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + ''));
+
+app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(8080, '0.0.0.0', function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Listening at http://localhost:8080');
+});
