0eca3834b6c3be7b714ada771d654ec23c030687
changgyun.woo <cg25.woo@samsung.com>
2016-09-01 18:16:18 +0900

    sortable
    Signed-off-by: changgyun.woo <cg25.woo@samsung.com>

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SortableList from '../components/sortable';
 
 require('../scss/main.scss');
+
+var data = {
+    items: [
+        "Gold",
+        "Crimson",
+        "Hotpink",
+        "Blueviolet",
+        "Cornflowerblue"
+    ]
+};
 
 class Home extends React.Component {
     render(){
         return (
             <div className="mainContainer">
-                <div className="mainBackground"></div>
+                <SortableList data={data} />
             </div>
         );
     }
